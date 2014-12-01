19428d5b8ed9fea09aaecffbe17021755262b4d2
Jordan <jorblume@gmail.com>
2014-12-01 14:03:06 -0800

    Jargonizer Tweaks
    Modified tweaks to jargonizer

--- substitutions/js/background.js
+++ substitutions/js/background.js
@@ -1,60 +1,111 @@
-//Default replacements
-var default_replacements = [
-    ['keyboard', 'leopard'],
-    ['keyboards', 'leopards'],
-    ['force', 'horse'],
-    ['forces', 'horses'],
-    ['witness', 'this dude I know'],
-    ['witnesses', 'these dudes I know'],
-    ['allegedly', 'kinda probably'],
-    ['new study', 'tumblr post'],
-    ['rebuild', 'avenge'],
-    ['space', 'spaaaaaace'],
-    ['google glass', 'virtual boy'],
-    ['smartphone', 'pok\u00E9dex'],
-    ['senator', 'elf-lord'],
-    ['senators', 'elf-lords'],
-    ['electric', 'atomic'],
-    ['car', 'cat'],
-    ['cars', 'cats'],
-    ['election', 'eating contest'],
-    ['elections', 'eating contests'],
-    ['congressional leaders', 'river spirits'],
-    ['homeland security', 'homestar runner'],
-    ['could not be reached for comment', 'is guilty and everyone knows it'],
-    ['batman', 'a man dressed like a bat'],
+//replacement array
+
+var replacement_array = [
+
+ ['Omni-channel',  'Hella-channel'],
+
+ ['CRM',  'Customer spying'],
+
+ ['The Internet of Things',  'Robot Apocalypse'], 
+
+ ['Internet of Things',  'Robot Apocalypse'], 
+
+ ['Crowdsourced', 'Lazysourced'],
+
+ ['crowdsourced', 'lazysourced'],
+
+ ['Crowdsourcing', 'Lazysourcing'],
+
+ ['crowdsourcing', 'lazysourcing'],
+
+['Media Agnostic', 'Media Don’t Care'], 
+
+['Paradigm shift', 'flip mode'],
+
+ ['Leverage', 'whore-out'],
+
+['Big data' , 'Math'],
+
+['Challenge the status quo', 'fight the man'], 
+
+['Disruptive', 'next-level'],
+ 
+['Game-changing', 'super dope'],
+
+['Game-changer', 'super dope'],
+ 
+['Guru' , 'smart motherfucker'],
+ 
+['Brand advocate' , 'fanboy'],
+
+['Brand community',  'fanboys'],
+
+['Mission' , 'goal'],
+
+['Asset' , 'stuff'],
+
+['Impression' ,  'Peep'], 
+ 
+['Impressions' ,  'Peeps'], 
+ 
+['Thought-leader', 'Smart motherfucker'],
+
+['Offer' ,  'bribe'],
+ 
+['Real-time' ,  'lickity-split'],
+ 
+['Influencer' ,  'popular kid'],
+ 
+['WOM' , 'Reco'],
+
+['Real-time bidding' , 'ad-buying robots'],
+
+['Programmatic buying' , 'ad-buying robots'],
+
+['Rich Media',  'Moving banners'],
+
+['Content',  'stuff'],
+
+['Native Advertising' , 'Camouflaged Advertising'],
+
+['Sponsored Content' , 'Camouflaged Advertising'],
+
+['influence' , 'brainwash'],
+
+["KPIs" , 'GOOOOOAAAAAALLLLLLS! '],
+
+['curated' , 'stole'],
+
+['curator' , 'stealer'],
+
+['curate' , 'steal'],
+
+['user-generated content' , "peoples stuff"],
+
+['call to action' , 'propaganda'],
+
+['respsonive design' , 'looks good on your phone'],
+
+['Consumer Engagement', 'Consumer Jibber Jabber']
+  
 ];
-//Default Blacklist
-var default_blacklisted_sites = ["docs.google.com",
-    "gmail.com",
-    "mail.google.com",
-    "mail.yahoo.com",
-    "outlook.com",
-]
 
-debug = false;
+debug = true;
 
-function checkBlackList(url, blacklist) {
-    url = url.toLowerCase() || "";
-    blacklist = blacklist || [];
-    for (var i = blacklist.length - 1; i >= 0; i--) {
-        if (url.indexOf(blacklist[i]) > -1) {
-            return false;
-        }
-    };
-    return true;
-}
+function inject(tabId, info, tab){
 
-function injectionScript(tabId, info, tab) {
-    if (debug) console.log("injection fire");
-    chrome.storage.sync.get(null, function(result) {
-        if (result["status"] === "enabled" && checkBlackList(tab.url, result['blacklist'])) {
-            chrome.tabs.executeScript(tabId, {
-                file: "js/substitutions.js",
-                runAt: "document_end"
-            });
-        }
-    });
+ 	if(debug) console.log('injecting now');
+
+	chrome.storage.sync.get(null, function(result) {
+		if(result['status'] === 'enabled' ){
+			chrome.tabs.executeScript(tabId, {
+
+				file: 'js/inject.js',
+				runAt: 'document_end'
+
+			});
+		}
+	});
 }
 
 function addMessage(request, sender, sendResponse) {
@@ -77,12 +128,7 @@ function fixDataCorruption() {
         }
         if (!result["replacements"]) {
             chrome.storage.sync.set({
-                "replacements": default_replacements
-            });
-        }
-        if (!result["replacements"]) {
-            chrome.storage.sync.set({
-                "blacklist": default_blacklisted_sites
+                "replacements": replacement_array
             });
         }
     });
@@ -98,18 +144,18 @@ function toggleActive() {
         }
         if (status === "enabled") {
             icon = {
-                "path": "images/disabled.png"
+                "path": "icons/disabled.jpg"
             };
             message = {
-                "title": "click to enable xkcd substitutions"
+                "title": "click to enable jargonizer"
             };
             status = "disabled";
         } else if (status === "disabled") {
             icon = {
-                "path": "images/enabled.png"
+                "path": "icons/enabled.jpg"
             };
             message = {
-                "title": "click to disabled xkcd substitutions"
+                "title": "click to disable jargonizer"
             };
             status = "enabled";
         }
@@ -123,6 +169,6 @@ function toggleActive() {
 
 chrome.browserAction.onClicked.addListener(toggleActive);
 chrome.runtime.onMessage.addListener(addMessage);
-chrome.tabs.onUpdated.addListener(injectionScript);
+chrome.tabs.onUpdated.addListener(inject);
 chrome.runtime.onInstalled.addListener(fixDataCorruption);
 chrome.runtime.onStartup.addListener(fixDataCorruption);
